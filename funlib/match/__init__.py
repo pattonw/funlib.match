dc07944881e7e50f8fdda851016dce0ded2a58af
pattonw <pattonw@hhmi.org>
2019-10-21 08:23:17 -0400

    initial commit

---
+++ funlib/match/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from .graph_to_tree_matcher import match_graph_to_tree
+
+
+__all__ = ['match_graph_to_tree']
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name='funlib.match',
+        version='0.1',
+        url='https://github.com/funkelab/funlib.match',
+        author='William Patton',
+        author_email='pattonw@hhmi.org',
+        license='MIT',
+        packages=[
+            'funlib.match'
+        ]
+)
