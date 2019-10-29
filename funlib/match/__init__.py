e191d66ecf1d0eef595ca8485af6978174797c13
pattonw <pattonw@hhmi.org>
2019-10-29 14:36:54 -0400

    add preprocessing to init file

--- funlib/match/__init__.py
+++ funlib/match/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from .graph_to_tree_matcher import match_graph_to_tree
+from .preprocess import mouselight_preprocessing
 
 
-__all__ = ['match_graph_to_tree']
+__all__ = ["match_graph_to_tree", "mouselight_preprocessing"]
